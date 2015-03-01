46b4caeeef73ffa8fc29058027d5d72f0be210a2
lightning11 <f35lightning2@sadalsuud.jp>
2015-03-01 22:27:46 +0900

    更新のテストまで作成

--- KabuWin2Test1/Test1Window.xaml.cs
+++ KabuWin2Test1/Test1Window.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using KabuWin2Dao;
 
 namespace KabuWin2Test1
 {
@@ -24,5 +25,54 @@ namespace KabuWin2Test1
         {
             InitializeComponent();
         }
+
+
+        private void SearchClicked(object sender, RoutedEventArgs e)
+        {
+
+            using (var context = new KabuContext())
+            {
+                // 検索
+                var list = from u in context.TKabuZandakas
+                           //where u.ShoukenKouzaCd == "02"
+                           select u;
+
+                List<TKabuZandaka> dataSource = new List<TKabuZandaka> { };
+
+                foreach (var l in list)
+                {
+                    Console.Out.WriteLine(l.MeigaraCd);
+
+                    dataSource.Add(new TKabuZandaka
+                    {
+                        Id = l.Id
+                        ,
+                        ShoukenKouzaCd = l.ShoukenKouzaCd
+                        ,
+                        MeigaraCd = l.MeigaraCd
+                        ,
+                        KabuSu = l.KabuSu
+                    });
+
+                }
+
+            }
+        }
+
+        private void UpdateClicked(object sender, RoutedEventArgs e)
+        {
+            using (var context = new KabuContext())
+            {
+
+                var zandaka = context.TKabuZandakas.Single(x => x.Id == 3);
+
+                zandaka.MeigaraCd = "1234";
+
+                context.SaveChanges();
+            }
+        }
+
+
+
     }
 }
