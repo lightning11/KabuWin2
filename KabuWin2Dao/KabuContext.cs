21ca71245bfeba14d9ad9a2e9b587fd67e90274a
lightning11 <f35lightning2@sadalsuud.jp>
2015-03-01 22:29:52 +0900

    更新処理のコミット漏れ追加

---
+++ KabuWin2Dao/KabuContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace KabuWin2Dao
+{
+    public class KabuContext : DbContext
+    {
+        public DbSet<TKabuZandaka> TKabuZandakas { get; set; }
+    }
+}
---
+++ KabuWin2Dao/TKabuZandaka.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KabuWin2Dao
+{
+    public class TKabuZandaka
+    {
+        public int Id { get; set; }
+        public string ShoukenKouzaCd { get; set; }
+        public string MeigaraCd { get; set; }
+        public Decimal KabuSu { get; set; }
+    }
+}
